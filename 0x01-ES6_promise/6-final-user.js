123a7b303bf6a51ae54bbd2094ef73f663d40364
Godwin <godwinchuks032@gmail.com>
2024-05-30 09:32:16 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,6 +7,6 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
     uploadPhoto(fileName),
   ]).then((results) => results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : String(result.value),
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
   })));
 }
