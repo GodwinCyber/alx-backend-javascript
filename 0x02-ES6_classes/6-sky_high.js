f5de28a50244fbf8968ebea659654ddd5cd5fd71
Godwin <godwinchuks032@gmail.com>
2024-05-31 19:47:43 +0000

    Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,18 +3,16 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft); // Assign sqft to the parent class Building
-    if (typeof floors !== 'number') {
-      throw new TypeError('floors must be a number');
-    }
+    this._sqft = sqft;
     this._floors = floors;
   }
 
-  // Getter for sqft
+  // Getter for floors
   get sqft() {
     return this._sqft;
   }
 
-  // Getter for floors
+  // Setter for floors
   get floors() {
     return this._floors;
   }
