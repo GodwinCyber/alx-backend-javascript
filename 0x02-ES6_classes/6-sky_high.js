1a99f470babd224aeef5d0e202a1638c3ec1682d
Godwin <godwinchuks032@gmail.com>
2024-05-31 19:50:08 +0000

    Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,22 +2,19 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft); // Assign sqft to the parent class Building
+    super(sqft);
     this._sqft = sqft;
     this._floors = floors;
   }
 
-  // Getter for floors
   get sqft() {
     return this._sqft;
   }
 
-  // Setter for floors
   get floors() {
     return this._floors;
   }
 
-  // Override the evacuationWarningMessage method
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors.`;
   }
