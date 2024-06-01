d91fc5eb80bc0af5cf2f024bd6f3b0dc87bde607
Godwin <godwinchuks032@gmail.com>
2024-06-01 00:07:45 +0000

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,12 +1,21 @@
 import Car from './10-car';
 
-export default class Evcar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color, range);
-    this._range = range;
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
   }
 
   cloneCar() {
     return new Car();
   }
 }
+EVCar[Symbol.species] = Car;
