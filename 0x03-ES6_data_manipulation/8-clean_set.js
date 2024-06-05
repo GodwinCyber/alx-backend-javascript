f58b23afc5621a10fed8c74108e865e29eb139ab
Godwin <godwinchuks032@gmail.com>
2024-06-05 04:27:39 +0000

    clean set by removing string that start with startString bon and finnaly join them

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
   return [...set].filter((item) => typeof item === 'string' && item.startsWith(startString))
     .map((item) => item.slice(startString.length))
     .join('-');
