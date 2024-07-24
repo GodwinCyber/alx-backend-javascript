ecd8810796ed09708adf603ec48939442d0e4a56
Godwin <godwinchuks032@gmail.com>
2024-07-24 08:30:13 +0100

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -26,34 +26,31 @@ const countStudents = (path) => {
     }
 
     const studentsByField = {};
+
     let totalStudents = 0;
 
-    for (let i = 1; i < lines.length; i++) {
+    for (let i = 1; i < lines.length; i += 1) {
       const line = lines[i].trim();
-
-      if (!line) continue;
-
       const [firstName, lastName, age, field] = line.split(',');
-      if (!firstName || !lastName || !age || !field) continue;
 
-      totalStudents += 1;
+      if (firstName && lastName && age && field) {
+        totalStudents += 1;
 
-      if (!studentsByField[field]) {
-        studentsByField[field] = [];
+        if (!studentsByField[field]) {
+          studentsByField[field] = [];
+        }
+        studentsByField[field].push(firstName);
       }
-
-      studentsByField[field].push(firstName);
     }
 
     console.log(`Number of students: ${totalStudents}`);
 
     for (const [field, students] of Object.entries(studentsByField)) {
-      console.log(
-        `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`
-      );
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
 };
+
 module.exports = countStudents;
