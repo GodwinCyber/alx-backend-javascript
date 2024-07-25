e137f1fe7b7dd3d0415360030085984ba7f8432a
Godwin <godwinchuks032@gmail.com>
2024-07-25 20:49:21 +0100

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,22 +1,28 @@
 const { expect } = require('chai');
+
 const getPaymentTokenFromAPI = require('./6-payment_token');
-const { response } = require('express');
 
 describe('getPaymentTokenFromAPI', () => {
-    it('should return a resolved promise with correct data with success is true', (done) => {
-        getPaymentTokenFromAPI(true)
-          .then((data) => {
-            expect(response).to.have,property('data');
-            expect(response.data).to.equal('Successful response from API');
-            done();
-          })
-          .catch((error) => {
-            done(error);
-          });
-    });
-    it('should do nothing when is false', (done) => {
-        const result = getPaymentTokenFromAPI(false);
-        expect(result).to.be.undefined;
+  it('should return a resolved promise with correct data when success is true', () => new Promise((done) => {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.have.property('data');
+        expect(response.data).to.equal('Successful response from the API');
         done();
-    });
+      })
+      .catch((error) => {
+        done(error);
+      });
+  }));
+
+  it('should return a promise that resolves to undefined when success is false', () => new Promise((done) => {
+    getPaymentTokenFromAPI(false)
+      .then((response) => {
+        expect(response).to.be.undefined;
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  }));
 });
