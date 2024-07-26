4407f397ee1a68aab4fb85e797871cd56a68ef14
Godwin <godwinchuks032@gmail.com>
2024-07-26 20:26:03 +0100

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const students = require('./3-read_file_async');
 
 const hostname = '127.0.0.1';
 const port = 1245;
@@ -7,33 +7,16 @@ const port = 1245;
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    const databaseFile = process.argv[2];
-
-    if (!databaseFile) {
-      res.writeHead(500);
-      res.end('Error: Database file path must be provided as an argument');
-      return;
-    }
-
-    countStudents(databaseFile)
-      .then((data) => {
-        res.write('This is the list of our students\n');
-        res.write(`Number of students: ${data.students.length}\n`);
-        res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-        res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-        res.end();
-      })
-      .catch((err) => {
-        res.writeHead(500);
-        res.end(`Error: ${err.message}`);
-      });
-  } else {
-    res.writeHead(404);
-    res.end('Not found');
+    res.write('This is the list of our students\n');
+    students(process.argv[2]).then((data) => {
+      res.write(`Number of students: ${data.students.length}\n`);
+      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+      res.end();
+    }).catch((err) => res.end(err.message));
   }
 });
 
