a237e12384afcfb21397581fbd2ee6d58452e394
Godwin <godwinchuks032@gmail.com>
2024-07-26 23:05:29 +0100

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,32 +1,47 @@
+// recreate the small HTTP server using Express:
+// It should be assigned to the variable app and this one must be exported
+// HTTP server should listen on port 1245
+// It should return plain text
+// When the URL path is /, it should display Hello Holberton School! in the page body
+// When the URL path is /students, it should display This is the list of our
+// students followed by the same content as the file 3-read_file_async.js
+// (with and without the database) - the name of the database
+// must be passed as argument of the file
+// CSV file can contain empty lines (at the end) - and they are not a valid student!
+
 const express = require('express');
-const students = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
-const hostname = '127.0.0.1';
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  await students(process.argv[2]).then((data) => {
-    res.write(`Number of students: ${data.students.length}\n`);
-    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-  }).catch((err) => res.write(err.message))
-    .finally(() => {
+app.get('/students', (req, res) => {
+  const databaseFile = process.argv[2];
+
+  if (!databaseFile) {
+    res.status(500).send('Error: Database file path must be provided as an argument');
+    return;
+  }
+
+  countStudents(databaseFile)
+    .then((data) => {
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${data.students.length}\n`);
+      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
       res.end();
+    })
+    .catch((err) => {
+      res.status(500).send(`Error: ${err.message}`);
     });
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
